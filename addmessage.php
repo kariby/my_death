ec6a932979bbd9d6d2d405a300cff0368e222ebd
kariby <43680637+kariby@users.noreply.github.com>
2021-02-19 09:48:15 +0500

    Update addmessage.php

--- addmessage.php
+++ addmessage.php
@@ -1,92 +1,92 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-<link rel="stylesheet" type="text/css" href="styles.css">
-<title>Передача показаний приборов учета в компанию Байрам</title>
+	<link href="styles.css" rel="stylesheet" type="text/css">
+	<title>Передача показаний приборов учета в компанию Байрам</title>
 </head>
 <body align="center" style="margin-top:30px;">
-<?php
-$servername = "localhost";
-$username = "id15961621_user";
-$password = "(v)hglN9YfsE0&1f";
-$database = "id15961621_bd_test20012021";
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-catch(PDOException $e) {
-    echo "Не удалось подключиться к БД. Попробуйте повторить попытку позднее <br>";
-    }
-$result_id = (int)$_POST['id_user'];
-if ($result_id >0):
-//этот код чтобы вернуться назад - ужас
-$sqli = "SELECT * FROM users WHERE user_id = ? LIMIT 1";
-$stmt= $conn->prepare($sqli);
-$stmt->execute(array($result_id));
-if (($stmt->rowCount())>0) {
-    while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
-        $log = $row['user_login'];
-        $pass = $row['user_password'];
-    }
-}
-?>
-<form method="POST" action="phpconnect.php">
-    <?php 
-    echo '<input type="hidden" name="user_login" value="'. $log .'">';
-    echo '<input type="hidden" name="user_password" value="'. $pass .'">';
-    ?>
-    <input type="submit" value="Назад" />
-</form>
-<?php
-//ужас закончился :)
-$result_type = $_POST['counter_type'];
-$result_number = $_POST['counter_number'];
-$result_res = (int)$_POST['result_number'];
-$result_date = date_create('now')->format('Y-m-d H:i:s');
-$sqli = "SELECT * FROM result WHERE result_id = ? AND result_number = ? ORDER BY result_date DESC LIMIT 1";
-$stmt= $conn->prepare($sqli);
-$stmt->execute([$result_id, $result_number]);
-//если таблица не пустая
-if (($stmt->rowCount()) > 0) {
-    //для каждого столбца таблицы result - я думаю нужно это будет удалить, у нас таблица result теперь из одной строки
-    foreach ($stmt as $row) {
-        if (($row['result_res'] >= $result_res) or (empty($result_number))) {
-            echo "<h2>Внимание! Произошла ошибка при вводе данных. <br>Повторите ввод корректных показаний</h2>";
-            break;
-        }
-        //если все показания в норме, то запишем их
-        else {
-            $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
-            $stmt= $conn->prepare($sql);
-            $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
-            echo "<h2>Показания внесены успешно, спасибо!</h2>";
-            break;
-        }
-    }
-}
-//если таблица пустая, то никаких проверок не нужно, забиваем сразу показания
-else {
-    $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
-    //проверка чтоб все показания были больше 0
-    if (($result_res>0) and (!empty($result_number))) {
-        $stmt= $conn->prepare($sql);
-        $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
-        echo "<h2><b>Показания внесены успешно, спасибо!</b></h2>";
-    }
-    else{
-        echo "<h2><b>Внимание! Произошла ошибка при вводе данных. <br>Повторите ввод корректных показаний</b></h2>";
-    }
-}
-?>
-<form method="post" action="index.php">
-    <input type="submit" value="Выход" />
-</form>
-<?php
-else:
-?>
-<h2 align="center">Не удалось войти в личный кабинет. <br>Повторите попытку авторизации!</h2>
-<form method="post" action="index.php"><input type="submit" value="Войти в личный кабинет"/></form>
-<?php
-endif;
-?>
+	<?php
+	$servername = "localhost";
+	$username = "id16171045_user";
+	$password = "%L5Ib~rowX|ta_EA";
+	$database = "id16171045_test";
+	try {
+	    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    }
+	catch(PDOException $e) {
+	    echo "Не удалось подключиться к БД. Попробуйте повторить попытку позднее <br>";
+	    }
+	$result_id = (int)$_POST['id_user'];
+	if ($result_id >0):
+	//этот код чтобы вернуться назад - ужас
+	$sqli = "SELECT * FROM users WHERE user_id = ? LIMIT 1";
+	$stmt= $conn->prepare($sqli);
+	$stmt->execute(array($result_id));
+	if (($stmt->rowCount())>0) {
+	    while ($row = $stmt->fetch(PDO::FETCH_LAZY)) {
+	        $log = $row['user_login'];
+	        $pass = $row['user_password'];
+	    }
+	}
+	?>
+	<form action="phpconnect.php" method="post">
+		<?php 
+		    echo '<input type="hidden" name="user_login" value="'. $log .'">';
+		    echo '<input type="hidden" name="user_password" value="'. $pass .'">';
+		    ?><input type="submit" value="Назад">
+	</form><?php
+	//ужас закончился :)
+	$result_type = $_POST['counter_type'];
+	$result_number = $_POST['counter_number'];
+	$result_res = (int)$_POST['result_number'];
+	$result_date = date_create('now')->format('Y-m-d H:i:s');
+	$sqli = "SELECT * FROM result WHERE result_id = ? AND result_number = ? ORDER BY result_date DESC LIMIT 1";
+	$stmt= $conn->prepare($sqli);
+	$stmt->execute([$result_id, $result_number]);
+	//если таблица не пустая
+	if (($stmt->rowCount()) > 0) {
+	    //для каждого столбца таблицы result - я думаю нужно это будет удалить, у нас таблица result теперь из одной строки
+	    foreach ($stmt as $row) {
+	        if (($row['result_res'] >= $result_res) or (empty($result_number))) {
+	            echo "<h2>Внимание! Произошла ошибка при вводе данных. <br>Повторите ввод корректных показаний</h2>";
+	            break;
+	        }
+	        //если все показания в норме, то запишем их
+	        else {
+	            $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
+	            $stmt= $conn->prepare($sql);
+	            $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
+	            echo "<h2>Показания внесены успешно, спасибо!</h2>";
+	            break;
+	        }
+	    }
+	}
+	//если таблица пустая, то никаких проверок не нужно, забиваем сразу показания
+	else {
+	    $sql = "INSERT INTO result (result_id, result_type, result_number, result_res, result_date) VALUES (?,?,?,?,?)";
+	    //проверка чтоб все показания были больше 0
+	    if (($result_res>0) and (!empty($result_number))) {
+	        $stmt= $conn->prepare($sql);
+	        $stmt->execute([$result_id, $result_type, $result_number, $result_res, $result_date]);
+	        echo "<h2><b>Показания внесены успешно, спасибо!</b></h2>";
+	    }
+	    else{
+	        echo "<h2><b>Внимание! Произошла ошибка при вводе данных. <br>Повторите ввод корректных показаний</b></h2>";
+	    }
+	}
+	?>
+	<form action="index.php" method="post">
+		<input type="submit" value="Выход">
+	</form><?php
+	else:
+	?>
+	<h2 align="center">Не удалось войти в личный кабинет.<br>
+	Повторите попытку авторизации!</h2>
+	<form action="index.php" method="post">
+		<input type="submit" value="Войти в личный кабинет">
+	</form><?php
+	endif;
+	?>
 </body>
+</html>
