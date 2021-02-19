97abc7cd1c37e3c0a08697bf596e46eb4f910ad9
kariby <43680637+kariby@users.noreply.github.com>
2021-02-19 09:47:10 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
- <link rel="stylesheet" type="text/css" href="styles.css">
- <title>Передача показаний приборов учета в компанию Байрам</title>
+	<link href="styles.css" rel="stylesheet" type="text/css">
+	<title>Передача показаний приборов учета в компанию Байрам</title>
 </head>
 <body style="margin-top:30px;">
-<div align="center">
-<h2>Внесение показаний приборов <br>учета для субарендаторов <br></h2>
-<a href="http://batyr-rb.ru/" target="_blank"><img src="http://www.batyr-rb.ru/upload/medialibrary/8d2/8d215ee628d5b088725c24b049a8018c.png" alt="Байрам" class="img-responsive" width="200"></a>
-<form action="phpconnect.php" method="post">
- <p>Логин: <br><input style="margin-top:5px;" type="text" name="user_login" /></p>
- <p>Пароль: <br><input style="margin-top:5px;" type="password" name="user_password" /></p>
- <p><input type="submit" value="Войти"/></p>
-</form>
-</div>
+	<div align="center">
+		<h2>Внесение показаний приборов<br>
+		учета для субарендаторов<br></h2><a href="http://batyr-rb.ru/" target="_blank"><img alt="Байрам" class="img-responsive" src="http://www.batyr-rb.ru/upload/medialibrary/8d2/8d215ee628d5b088725c24b049a8018c.png" width="200"></a>
+		<form action="phpconnect.php" method="post">
+			<p>Логин:<br>
+			<input name="user_login" style="margin-top:5px;" type="text"></p>
+			<p>Пароль:<br>
+			<input name="user_password" style="margin-top:5px;" type="password"></p>
+			<p><input type="submit" value="Войти"></p>
+		</form>
+	</div>
 </body>
 </html>
